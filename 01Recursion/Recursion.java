d5a89e68bb9a75a5a86e86e499e8108205259524
Karen Li <kli19@stuy.edu>
2018-02-03 20:51:23 -0500

    tested fib and factorial with negatives

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -29,13 +29,29 @@ public class Recursion{
 	return fib(n-1) + fib(n-2);
     }
 
+    public static double sqrt(double n){
+	//error
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+
+	double guess = 1;
+	return guess;
+    }
+
+    public static double sqrtHelper(){
+	return 0.0;
+    }
+
     public static void main(String[]args){
 	//testing fact
+	//System.out.println(fact(-1));
 	//System.out.println (fact(0)); // 1
 	//System.out.println (fact(1)); // 1
 	//System.out.println (fact(5)); // 120
 
 	//testing fib
+	System.out.println(fib(-1));
 	//System.out.println (fib(0)); // 0
 	//System.out.println (fib(1)); // 1
 	//System.out.println (fib(9)); // 34
