a7303a93dbc5e83272b4aa54084854d4ec856422
Karen Li <kli19@stuy.edu>
2018-02-03 20:19:09 -0500

    fact works on 0

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,5 +1,18 @@
 public class Recursion{
-    public int fact(int n){
+    public static int fact(int n){
+	
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+
+	if (n == 0){
+	    return 1;
+	}
+
+	return fact(n-1) * n;
     }
-    
+
+    public static void main(String[]args){
+	System.out.println (fact(0));
+    }
 }
