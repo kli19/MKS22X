c58c8e033ddb52be4813f53e3f7662deb3ceb315
Karen Li <kli19@stuy.edu>
2018-02-03 20:29:27 -0500

    fib works on base cases and recursive cases

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,22 +1,49 @@
 public class Recursion{
     public static int fact(int n){
-	
+	//error
 	if (n < 0){
 	    throw new IllegalArgumentException();
 	}
-
+	
+	//base case
 	if (n == 0){
 	    return 1;
 	}
-
+	
+	//recursive case
 	return fact(n-1) * n;
     }
 
+    public static int fib(int n){
+	//error
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	
+	//base case 0
+	if (n == 0){
+	    return 0;
+	}
+
+	//base case 1
+	if (n == 1){
+	    return 1;
+	}
+	
+	//recursive case
+	return fib(n-1) + fib(n-2);
+    }
+
     public static void main(String[]args){
-	System.out.println (fact(0)); // 1
-	System.out.println (fact(1)); // 1
-	System.out.println (fact(2)); // 2
-	System.out.println (fact(5)); // 120
+	//testing fact
+	//System.out.println (fact(0)); // 1
+	//System.out.println (fact(1)); // 1
+	//System.out.println (fact(5)); // 120
+
+	//testing fib
+	System.out.println (fib(0)); // 0
+	System.out.println (fib(1)); // 1
+	System.out.println (fib(9)); // 34
 	
     }
 }
