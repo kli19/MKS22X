e4c5938090789ad73b4c882fe049419bb6230531
kli19 <kli19@stuy.edu>
2018-02-09 15:01:51 -0500

    initial commit with skeleton functions

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,28 @@
+public class QueenBoard{
+    private int[][]board;
+
+    public QueenBoard(int size){
+	
+    }
+
+    private boolean addQueen(int r, int c){
+	return false;
+    }
+
+    private boolean removeQueen(int r, int c){
+	return true;
+    }
+
+    public String toString(){
+	return "";
+    }
+
+    public boolean solve(){
+	return true;
+    }
+
+
+    public int countSolutions(){
+	return 0;
+    }
+}
