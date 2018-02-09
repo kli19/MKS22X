2846135507184ebc38a970cbed708af24af182a9
kli19 <kli19@stuy.edu>
2018-02-09 15:14:43 -0500

    tested toString and addQueen

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -2,19 +2,37 @@ public class QueenBoard{
     private int[][]board;
 
     public QueenBoard(int size){
-	
+	board = new int[size][size];
     }
 
     private boolean addQueen(int r, int c){
+	if (board[r][c] == 0){
+	    board[r][c] = -1;
+	    return true;
+	}
 	return false;
     }
 
     private boolean removeQueen(int r, int c){
-	return true;
+	return false;
     }
 
     public String toString(){
-	return "";
+	String str = "";
+
+	for (int i = 0; i < board.length; i ++){
+	    for (int j = 0; j < board[i].length; j++){
+		if (board[i][j] == -1){
+		    str += "Q ";
+		}
+		else{
+		    str += "_ ";
+		}
+	    }
+	    str += "\n";
+	}
+	
+	return str;
     }
 
     public boolean solve(){
@@ -24,5 +42,11 @@ public class QueenBoard{
 
     public int countSolutions(){
 	return 0;
+    }
+
+    public static void main(String[]args){
+	QueenBoard x = new QueenBoard(5);
+	x.addQueen(2,3);
+	System.out.println(x);
     }
 }
