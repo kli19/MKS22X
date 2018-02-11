c30848d5d985e096009425e28719fc8d41b7b3a8
Karen Li <kli19@stuy.edu>
2018-02-10 22:31:36 -0500

    comments to clarify functionality

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -14,16 +14,19 @@ public class QueenBoard{
 
 	    for (int i = c + 1; i < board.length; i++){
 		board[r][i] += 1;
+		//for testing
 		//System.out.println(toString());
 	    }
 
 	    for (int i = r+1, j = c+1; i < board.length && j < board.length; i++, j++){
 		board[i][j] += 1;
+		//for testing
 		//System.out.println(toString());
 	    }
 
 	    for (int i = r-1, j = c+1; i >= 0 && j < board.length; i--, j++){
 		board[i][j] += 1;
+		//for testing
 		//System.out.println(toString());
 	    }	    
 	    
@@ -54,6 +57,15 @@ public class QueenBoard{
 	return false;
     }
 
+    /**
+     *@return The output string formatted as follows:
+     *All numbers that represent queens are replaced with 'Q' 
+     *all others are displayed as underscores '_'
+     *There are spaces between each symbol:  
+     *Q _ _ _
+     *_ _ Q _
+     * etc.
+     */
     public String toString(){
 	String str = "";
 
@@ -72,6 +84,11 @@ public class QueenBoard{
 	return str;
     }
 
+    /**
+     *@return false when the board is not solveable and leaves the board filled with zeros; 
+     *        true when the board is solveable, and leaves the board in a solved state
+     *@throws IllegalStateException when the board starts with any non-zero value
+     */
     public boolean solve(){
 	for(int r = 0; r < board.length; r++){
 	    for(int c = 0; c < board.length; c++){
@@ -100,6 +117,10 @@ public class QueenBoard{
 	return false;
     }
 
+    /**
+     *@return the number of solutions found, and leaves the board filled with only 0's
+     *@throws IllegalStateException when the board starts with any non-zero value
+     */
     public int countSolutions(){
 	for (int r = 0; r < board.length; r++){
 	    for (int c = 0; c < board.length; c++){
@@ -126,6 +147,8 @@ public class QueenBoard{
     }
 
     public static void main(String[]args){
+
+	
 	QueenBoard x = new QueenBoard(3);
 	System.out.println(x.countSolutions());
 	System.out.println(x.solve());
@@ -167,5 +190,6 @@ public class QueenBoard{
 	     */
 	     System.out.println(a); //prints out an empty i by i grid of underscores
 	 }
+    
     }
 }
