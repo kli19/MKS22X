881d08e576e18ca27e9d4d58f6a51851920bf8e5
kli19 <kli19@stuy.edu>
2018-02-12 15:24:55 -0500

    deleted some tests from main

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -112,7 +112,7 @@ public class QueenBoard{
 		//uncomment this code for testing
 		//System.out.println(Text.go(1,1));
 		//System.out.println(this);
-		//Text.wait(1000);
+		//Text.wait(900);
 		if (solveHelp(column + 1)){
 		    return true;
 		}
@@ -154,13 +154,40 @@ public class QueenBoard{
 
     public static void main(String[]args){
 
-	
-	QueenBoard x = new QueenBoard(3);
-	System.out.println(x.countSolutions());
-	System.out.println(x.solve());
-        System.out.println(x);
+//--------------thanks for the test cases Crystal---------------
+	QueenBoard b = new QueenBoard(4);
+
+	System.out.println(b.solve()); //prints true
+	System.out.println(b); //prints a valid solution
 
 
+	try{
+	    b.solve();
+	}catch(IllegalStateException e){
+	    System.out.println("Error: The board contains non-zero values");
+	} //prints "Error: The board contains non-zero values"
+
+
+	try{
+	    b.countSolutions();
+	}catch(IllegalStateException e){
+	    System.out.println("Error: The board contains non-zero values");
+	} //prints "Error: The board contains non-zero values"
+
+	for (int i = 0; i < 12; i++){
+	    QueenBoard a = new QueenBoard(i);
+	    System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
+	    /*          Expected Values
+			i --> # of Solutions   i --> # of Solutions
+			0 --> 1                      6 --> 4
+			1 --> 1                      7 --> 40
+			2 --> 0                      8 --> 92
+			3 --> 0                      9 --> 352
+			4 --> 2                    10 --> 724
+			5 --> 10                  11 --> 2680
+	    */
+	    System.out.println(a); //prints out an empty i by i grid of underscores
+}
 
 
     }
