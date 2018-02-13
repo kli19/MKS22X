5ee4fd606a3e243addd49fb9d1984ee69e8ae9a8
kli19 <kli19@stuy.edu>
2018-02-13 15:13:53 -0500

    exception in constructor

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -3,7 +3,15 @@ public class KnightBoard{
     private int[][] board;
     
     public KnightBoard(int startingRows, int startingCols){
+	if (startingRows < 0 || startingCols < 0){
+	    throw new IllegalArgumentException("Parameters cannot be negative");
+	}
+	
 	board = new int[startingRows][startingCols];
+    }
+
+    public String toString(){
+	
     }
 
 }
