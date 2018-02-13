a9cfc2da0fd9cf563f14ee338d660769cbab135b
kli19 <kli19@stuy.edu>
2018-02-13 15:30:04 -0500

    wrote and tested toString

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -8,10 +8,38 @@ public class KnightBoard{
 	}
 	
 	board = new int[startingRows][startingCols];
+	
+	//testing toString
+	/*
+	for (int r = 0; r < board.length; r++){
+	    for (int c = 0; c < board[0].length; c++){
+		board[r][c] = c+7;
+	    }
+	}
+	*/
     }
 
     public String toString(){
-	
+	String str = "";
+	for (int r = 0; r < board.length; r++){
+	    for (int c = 0; c < board[0].length; c++){
+		if (board[r][c] == 0){
+		    str += "_ " ;
+		}
+		else if (board[r][c] < 10){
+		    str += " " + board[r][c] + " ";
+		}
+	        else {
+		    str += board[r][c]+ " " ;
+		}		
+	    }
+	    str+= "\n";
+	}
+	return str;
     }
 
+    public static void main(String[]args){
+	KnightBoard a = new KnightBoard(5,5);
+	System.out.println(a);
+    }
 }
