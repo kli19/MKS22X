77761c82f2a8605c5a483a97f6fbf028e9b40c69
Karen Li <kli19@stuy.edu>
2018-03-05 22:05:49 -0500

    basic skeleton

---
+++ 05USACO/USACO.java
@@ -0,0 +1,9 @@
+public class USACO {
+    public static int bronze(String filename){
+	return 0; // so it compiles
+    }
+
+    public static int silver(String filename){
+	return 0; // so it compiles
+    }
+}
