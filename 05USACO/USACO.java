3e9da4dbb49c2037c66230018dfca32400709918
Karen Li <kli19@stuy.edu>
2018-03-05 23:37:22 -0500

    finished i think

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -4,23 +4,41 @@ import java.util.*;
 public class USACO {
     public static int bronze(String filename){
 	try {
-	    File text = new File(filename);
-	    Scanner input = new Scanner(text);
+	    
+	    File text = new File(filename);	    
+	    Scanner input = new Scanner(text);	  
 
 	    int R = input.nextInt();
 	    int C = input.nextInt();
 	    int E = input.nextInt();
-	    int N = input.nextInt();
+	    int N = input.nextInt();	    
 
 	    int[][] lake = new int[R][C];
 	    for (int r = 0; r < R; r++){
-		for (int c = 0; c < C; r++){
+		for (int c = 0; c < C; c++){
 		    lake[r][c] = input.nextInt();
 		}
 	    }
-	}catch(Exception e){}
 
+	    for (int n = N; n > 0; n--){
+		int R_s = input.nextInt() -1;
+		int C_s = input.nextInt() -1;
+		int D_s = input.nextInt();
+		stomp(lake, R_s, C_s, D_s);		
+	    }
 
+	    int totalDepth = 0;
+	    for (int r = 0; r < R; r ++){
+		for (int c = 0; c < C; c++){
+		    if (E > lake[r][c]){
+			totalDepth += E - lake[r][c];
+		    }
+		}
+	    }
+	   
+	    return totalDepth * 72 *72;
+	    
+	}catch(Exception e){System.exit(1);}
 	
 	return 0; // so it compiles
     }
@@ -32,10 +50,10 @@ public class USACO {
 	    {2,0}, {2,1}, {2,2}
 	};
 
-	int max = 0;
-
-	for (int d = D_s; d > 0; d --){
 	
+
+	for (int d = D_s; d > 0; d--){
+	int max = 0;
 	    for (int i[]: cowLocations){
 		if (lake[R_s + i[0]][C_s + i[1]] > max){
 		    max = lake[R_s + i[0]][C_s + i[1]];
@@ -47,12 +65,28 @@ public class USACO {
 		    lake[R_s + i[0]][C_s + i[1]]--;
 		}
 	    }
-
 	}
+	//for debugging
+	//System.out.println(toString(lake));
 	
     }
     
     public static int silver(String filename){
 	return 0; // so it compiles
+    }
+
+    public static String toString(int[][] arr){
+	String str = "";
+	for (int r = 0; r < arr.length; r++){
+	    for (int c = 0; c < arr[r].length; c++){
+		str += arr[r][c] + " ";
+	    }
+	    str += "\n";
+	}
+	return str;
+    }
+
+    public static void main (String[]args){
+	System.out.println(bronze("bronzeTest1.txt"));
     }
 }
