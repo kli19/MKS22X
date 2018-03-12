294a43b9e8de32b2b613a4946632adee00c52ac1
Karen Li <kli19@stuy.edu>
2018-03-12 19:20:17 -0400

    toString and swap

--- 06QuickSelect/Quick.java
+++ 06QuickSelect/Quick.java
@@ -4,11 +4,30 @@ public class Quick{
     public static int partition (int[] data, int start, int end){
 	Random rand = new Random();
 	int n = rand.nextInt(end - start + 1) + start;
-	return 0;
+	return n;
+    }
+
+    public static void swap (int[] arr, int n, int m){
+	int hold = arr[n];
+	arr[n] = arr[m];
+	arr[m] = hold;
+    }
+
+    public static String toString(int[] arr){
+	String str = "{";
+	for (int i = 0; i < arr.length; i ++){
+	    str += arr[i];
+	    if (i < arr.length -1){
+		str += ", ";
+	    }
+	}
+	str += "}";
+	return str;
     }
 
     public static void main(String[]args){
 	int[] x = new int[] {17, 61, 67, 47, 93 ,12, 20, 4, 44,	68};
-	System.out.println(partition(x, 0, 9)); // 
+	System.out.println(partition(x, 0, 9));
+	System.out.println(toString(x));
     }
 }
