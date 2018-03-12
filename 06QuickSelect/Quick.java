61d0ff0a10b5e8dc16c847f952ef5c0eb224d662
Karen Li <kli19@stuy.edu>
2018-03-12 19:08:12 -0400

    skeleton

---
+++ 06QuickSelect/Quick.java
@@ -0,0 +1,11 @@
+public class Quick{
+
+    public static int partition (int[] data, int start, int end){
+	return 0;
+    }
+
+    public static void main(String[]args){
+	int[] x = new int[] {17, 61, 67, 47, 93 ,12, 20, 4, 44,	68};
+	System.out.println(partition(x, 0, 9));
+    }
+}
