635cbfb5e5170984fafbcc4a4035063243b2a029
Karen Li <kli19@stuy.edu>
2018-03-12 19:12:34 -0400

    random

--- 06QuickSelect/Quick.java
+++ 06QuickSelect/Quick.java
@@ -1,11 +1,14 @@
+import java.util.Random;
 public class Quick{
 
     public static int partition (int[] data, int start, int end){
+	Random rand = new Random();
+	int n = rand.nextInt(end - start + 1) + start;
 	return 0;
     }
 
     public static void main(String[]args){
 	int[] x = new int[] {17, 61, 67, 47, 93 ,12, 20, 4, 44,	68};
-	System.out.println(partition(x, 0, 9));
+	System.out.println(partition(x, 0, 9)); // 
     }
 }
