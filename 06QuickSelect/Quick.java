fa7b75e2d4553e20880008fc3e143748f49252f7
Karen Li <kli19@stuy.edu>
2018-03-13 20:12:19 -0400

    quickselect

--- 06QuickSelect/Quick.java
+++ 06QuickSelect/Quick.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 public class Quick{
 
-    public static int partition (int[] data, int start, int end){
+    public static int part (int[] data, int start, int end){
 	Random rand = new Random();
 	int n = rand.nextInt(end - start + 1) + start;
 
@@ -26,6 +26,22 @@ public class Quick{
 	return e;
     }
 
+    public static int quickselect(int[] data, int k){
+	int start = 0;
+	int end = data.length-1;
+	int index = part(data, start, end);
+	while (index != k){
+	    if (index < k){
+		start = index+1;
+	    }
+	    else if (index > k){
+		end = index-1;
+	    }
+	    index = part(data, start, end);
+	}
+	return data[index]; //so it compiles
+    }
+
     public static void swap (int[] arr, int n, int m){
 	int hold = arr[n];
 	arr[n] = arr[m];
@@ -45,8 +61,13 @@ public class Quick{
     }
 
     public static void main(String[]args){
-	//int[] x = new int[] {17, 61, 67, 47, 93 ,12, 20, 4, 44,	68};
-	//System.out.println(partition(x, 0, 9));
-	//System.out.println(toString(x));
+	int[] x = new int[] {17, 61, 67, 47, 93 ,12, 20, 4, 44};
+	//System.out.println(part(x, 0, 9));
+	for (int i = 0; i < x.length; i++){
+	    System.out.println(quickselect(x, i));
+	    System.out.println(toString(x));
+	}
+	
+	
     }
 }
