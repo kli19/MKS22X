7412bcb2c13451ad436b87aa6eb4f2fd35e70c9c
Karen Li <kli19@stuy.edu>
2018-04-15 01:37:47 -0400

    radix works for negative nums

--- 08Linked/Sort.java
+++ 08Linked/Sort.java
@@ -1,27 +1,45 @@
+import java.util.*;
 public class Sort{
+  
     public static void radixsort(MyLinkedListImproved<Integer> data){
 	if (data.size() > 0){
-	    @SuppressWarnings("unchecked") 
-	    MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
+	    @SuppressWarnings("unchecked")
+		MyLinkedListImproved<Integer>[] positiveBucket = new MyLinkedListImproved[10];
+	    @SuppressWarnings("unchecked")
+		MyLinkedListImproved<Integer>[] negativeBucket = new MyLinkedListImproved[10];
 
-	    for(int i = 0; i < bucket.length; i++){
-                bucket[i] = new MyLinkedListImproved<Integer>();
+	    for(int i = 0; i < 10; i++){
+                positiveBucket[i] = new MyLinkedListImproved<Integer>();
+		negativeBucket[i] = new MyLinkedListImproved<Integer>();
             }
 
-	    int maxNumDigits = (data.get(data.max()) + "").length();
+	    int max = Math.abs(data.get(data.max()));		
+	    int min = Math.abs(data.get(data.min()));
+	    int maxNumDigits = (Math.max(max, min) + "").length();
 
-	    for (int i = 0; i < maxNumDigits; i++){
+	    for (int i = 0; i < maxNumDigits; i++){		
 		for (Integer num: data){
-		    int index = (int)(num / Math.pow(10, i)) % 10;
-		    //System.out.println ("index: " + index);
-		    bucket[index].add(num);		    
+		    if(num >= 0){	    
+			int index = (int)(num / Math.pow(10, i)) % 10;
+			positiveBucket[index].add(num);
+		    }
+
+		    else{
+			int index = Math.abs(9-(int)(num*-1 / Math.pow(10, i)) % 10);
+			negativeBucket[index].add(num);
+		    }
 		}
 		data.clear();
 		
-		for (int j = 0; j < bucket.length; j++){
-		    data.extend(bucket[j]);
-		    bucket[j].clear();
+		for (int j = 0; j < negativeBucket.length; j++){
+		    data.extend(negativeBucket[j]);
+		    //System.out.println(data);
 		}
+		for (int j = 0; j < positiveBucket.length; j++){
+		    data.extend(positiveBucket[j]);
+		    //System.out.println(data);
+		}		
+
 		
 	    }
 	    
@@ -29,16 +47,21 @@ public class Sort{
 
     }
 
+    /*
     public static void main(String[]args){
 	MyLinkedListImproved<Integer> m = new MyLinkedListImproved<>();       
 	
 	for (int i = 10; i>0; i--){
 	    m.add(Integer.valueOf(i)); 
 	}
+	m.add(-5);
+	m.add(-1);
 	System.out.println(m);
 
 	radixsort(m);
 	System.out.println(m);
 	
     }
+    */
+
 }   
