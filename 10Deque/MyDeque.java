983bf7d9fc9bd0191243fed533248e3068613e1d
kli19 <kli19@stuy.edu>
2018-04-19 15:36:08 -0400

    lab

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -2,18 +2,23 @@ import java.util.*;
 public class MyDeque<E>{
 
     private E[] data;
-    private int length;
+    private int length, first, last;
+    
 
     @SuppressWarnings("unchecked")
     public MyDeque(){
 	data = (E[])new Object[10];
 	length = 0;
+	first = 0;
+	last = 0;
     }
     
     @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
         data = (E[])new Object[initialCapacity];
 	length = 0;
+	first = 0;
+	last = 0;
     }
 
     public int size(){
@@ -24,11 +29,28 @@ public class MyDeque<E>{
 	if (thing == null){
 	    throw new NullPointerException();
 	}
+	
+	else if (size() == data.length){
+	    resize();
+	}
+
+	else {
+	    if(first == 0){
+		data[data.length-1] = thing;
+	    }
+	
+	    else{
+		data[first-1] = thing;
+	    }
+	}
     }
 
     public void addLast(E thing){
 	if (thing == null){
 	    throw new NullPointerException();
+	}
+	else if (size() == data.length){
+	    resize();
 	}
     }
 
@@ -60,4 +82,50 @@ public class MyDeque<E>{
 	return null;
     }
 
+    @SuppressWarnings("unchecked")
+    public void resize(){
+	E[] temp = (E[])new Object[size()*2];
+
+	int index = 0;
+	for (int i = first; i < data.length; i++){
+	    temp[index] = data[i];
+	    index++;
+	}
+	
+	if (first > 0){
+	    for (int i = 0; i < last; i++){
+		temp[index] = data[i];
+		index++;
+	    }
+	}
+	
+	first = 0;
+	last = size()-1;   
+    }
+
+    public String toString(){
+	String str = "";
+	int index = 0;
+	for (int i = first; i < data.length; i++){
+	    str += data[i];
+	    index++;
+	}
+	
+	if (first > 0){
+	    for (int i = 0; i < last; i++){
+	        str += data[i];
+		index++;
+	    }
+	}
+	return str;
+
+	
+    }
+
+    public static void main(String[]args){
+	MyDeque<Integer>  x = new MyDeque<>();
+	x.addFirst(21);
+	System.out.println(x);
+    }
+ 
 }
