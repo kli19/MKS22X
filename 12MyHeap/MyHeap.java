a72e318a00b6a44cdc4285b796ca660170b925b6
Karen Li <kli19@stuy.edu>
2018-04-26 23:40:41 -0400

    'miscellaneous

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -4,15 +4,18 @@ public class MyHeap{
     private boolean isMax;
 
     public MyHeap(){
-	isMax = true;
-	data = new String[10];
+	this(true);
     }
 
     public MyHeap(boolean b){
-	this();
-	if (!b){
+	if (b){
+	    isMax = true;
+	}
+	else{
 	    isMax = false;
 	}
+	data = new String[10];
+	length = 0;
     }
 
     public int size(){
@@ -23,14 +26,44 @@ public class MyHeap{
 	return data[0];
     }
 
+    public void pushUp(int index){
+	swap(index, (index-1)/2);
+    }
+
+    public void pushDownLeft(int index){
+	swap(index, index*2+1);
+    }
+
+    public void pushDownRight(int index){
+	swap(index, index*2+2);
+    }
+
     public void add(String s){
 	
+	length++;
+    }
+
+    public String remove(){
+	String s = data[0];
+
+	swap(0, size()-1);
+	
+	length--;
+	return s;
     }
 
     public void swap (int n, int m){
 	String temp = data[n];
 	data[n] = data[m];
 	data[m] = temp;
+    }
+
+    public String toString(){
+	String str = "";
+	for (int i = 0; i < size(); i++){
+	    str += data[i] + "  ";
+	}
+	return str;
     }
    
 }
