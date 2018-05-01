fa35c8e24e4309382267fd24a7ab6fb75807bfa5
Karen Li <kli19@stuy.edu>
2018-04-30 23:37:10 -0400

    getMedian works

--- 13Medians/RunningMedians.java
+++ 13Medians/RunningMedians.java
@@ -34,15 +34,32 @@ public class RunningMedians{
 	}
     }
 
+    public double getMedian(){
+	if (small.size()>large.size()){
+	    return small.peek();
+	}
+
+	else if(large.size()> small.size()){
+	    return large.peek();
+	}
+	
+	return ((double)small.peek()+(double)large.peek()) / 2;	
+    }
+
     public static void main(String[]args){
+	/*
 	RunningMedians x = new RunningMedians();
 
 	x.add(1);
 	x.add(2);
-	x.add(3);
+	x.add(10);
 	x.add(7);
+	x.add(0);
+	x.add(5);
 	System.out.println("small: " + x.getSmall());
 	System.out.println("large: " + x.getLarge());
+	System.out.println("median: " + x.getMedian());
+	*/
     }
     
 }
