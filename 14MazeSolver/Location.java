aaada2f3fd4d9cf9ea9aa595cd8ceb9fa9d6d0b1
kli19 <kli19@stuy.edu>
2018-05-04 15:07:18 -0400

    Location fields and constructor

--- 14MazeSolver/Location.java
+++ 14MazeSolver/Location.java
@@ -1,15 +1,10 @@
-public class Location{
-
-    private int row;
-    private int col;
+`public class Location{
+    private int x,y;
     private Location previous;
-    private int distanceToStart;
 
-    public Location(int row, int col, Location previous, int distanceToStart){
-	this.row = row;
-	this.col = col;
-	this.previous = previous;
-	this.distanceToStart = distanceToStart;
+    public Location(int _x, int _y, Location prev){
+	x = _x;
+	y = _y;
+	previous = prev;
     }
-    
 }
