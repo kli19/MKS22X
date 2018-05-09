60ffb166b1b43b206dbc7e5809e8529ac7ca8a46
Karen Li <kli19@stuy.edu>
2018-05-08 20:27:59 -0400

    stack

---
+++ 14MazeSolver/FrontierStack.java
@@ -0,0 +1,20 @@
+public class FrontierStack implements Frontier{
+
+    private Stack<Location> data;
+
+    public FrontierStack(){
+	data = new Stack<Location>();
+    }
+
+    public Location next(){
+	return data.pop();
+    }
+
+    public void add(Location n){
+	data.add(n);
+    }
+
+    public boolean hasNext(){
+	return data.size() != 0;
+    }
+}
